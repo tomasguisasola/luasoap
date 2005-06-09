4893be3def11ffc18bb7cc946267b10ce77fa4ba
mascarenhas <mascarenhas>
2005-06-09 21:07:47 +0000

    Updated http.lua for LuaSocket 2.0 beta3.

--- src/http.lua
+++ src/http.lua
@@ -1,33 +1,41 @@
 ---------------------------------------------------------------------
 -- SOAP over HTTP.
 -- See Copyright notice in license.html
--- $Id: http.lua,v 1.1 2004/03/24 19:27:37 tomas Exp $
+-- $Id: http.lua,v 1.2 2005/06/09 21:07:47 mascarenhas Exp $
 ---------------------------------------------------------------------
 
-require"luasocket"
+require"socket.http"
+require"ltn12"
 require"soap"
 
-local post = socket.http.post
+local request = socket.http.request
 
-soap.http = {}
+module("soap.http")
 
 ---------------------------------------------------------------------
 -- Call a remote method.
 -- @param url String with the location of the server.
 -- @param namespace
 ---------------------------------------------------------------------
-function soap.http.call (url, namespace, method, entries, headers)
-	local body, headers, code, err = post {
+function call (url, namespace, method, entries, headers)
+        local request_sink, tbody = ltn12.sink.table()
+	local request_body = soap.encode(namespace, method, entries, headers)
+	print(request_body)
+	local err, code, headers, status = request {
 		url = url,
-		body = soap.encode (namespace, method, entries, headers),
+		method = "POST",
+		source = ltn12.source.string(request_body),
+		sink = request_sink,
 		headers = {
-			["Content-type"] = "text/xml",
+			["Content-Type"] = "text/xml",
+			["content-length"] = tostring(string.len(request_body)),
 			["SOAPAction"] = '"'..method..'"',
 		},
 	}
+	local body = table.concat(tbody)
 	if tonumber (code) == 200 then
 		return soap.decode (body)
 	else
-		error ((err or code).."\n\n"..body)
+		error (tostring(err or code).."\n\n"..tostring(body))
 	end
 end
