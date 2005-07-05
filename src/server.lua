7fb9ab59af796d2f952909f25a15e98e38f35ce1
tomas <tomas>
2005-07-05 16:34:41 +0000

    Cleanup.

--- src/server.lua
+++ src/server.lua
@@ -43,40 +43,6 @@ local function respond (resp)
 end
 
 ---------------------------------------------------------------------
---[[
-function call (url, namespace, method, entries, headers)
-    local request_sink, tbody = ltn12.sink.table() 
-    local request_body = soap.encode(namespace, method, entries, headers)
-    local err, code, headers, status = request {
-        url = url,
-        method = "POST",
-        source = ltn12.source.string(request_body),
-        sink = request_sink,
-        headers = {
-			["Content-Type"] = "text/xml",
-			["content-length"] = tostring(string.len(request_body)),
-			["SOAPAction"] = '"'..method..'"',
-        },
-    }
-    local body = table.concat(tbody)
-    if tonumber (code) == 200 then 
-        return soap.decode (body)
-    else
-        error (tostring(err or code).."\n\n"..tostring(body))
-    end
-end 
-
-function assert (cond, msg)
-	if not cond then
-		respond (xmlrpc.srvEncode (
-			{ code = 2, message = msg, },
-			true
-		))
-		os.exit() -- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	end
-end
---]]
----------------------------------------------------------------------
 function builderrorenvelope (faultcode, faultstring, extra)
 	return soap.encode (nil, "SOAP-ENV:Fault", {
         { tag = "faultcode", faultcode, },
@@ -88,45 +54,12 @@ end
 ---------------------------------------------------------------------
 cgilua.seterroroutput (function (msg)
 	respond (builderrorenvelope ("SOAP-ENV:Server", msg))
---[[
-	respond (soap.encode (nil, "SOAP-ENV:Fault", {
-        { tag = "faultcode", "SOAP-ENV:Server", },
-        { tag = "faultstring", "Server Error", },
-        {
-            tag = "detail",
-            {
-                tag = "e:myfaultdetails",
-                attr = { "xmlns:e", ["xmlns:e"] = "Some-URI", },
-                { tag = "message", msg, },
-                { tag = "errorcode", 1001, },
-            },
-        },
-    }))
---]]
 end)
 
 ---------------------------------------------------------------------
 local function decodedata (doc)
---[[
-require"cgilua.serialize"
-cgilua.errorlog("xmldoc:\n")
-cgilua.errorlog(type(doc).."\n")
-cgilua.errorlog(doc.."\n")
---]]
 	local namespace, elem_name, elems = soap.decode (doc)
---[[
-cgilua.errorlog("namespace: "..tostring(namespace))
-cgilua.errorlog("\n")
-cgilua.errorlog("elem_name: "..tostring(elem_name))
-cgilua.errorlog("\n")
-cgilua.errorlog("elems: ")
-cgilua.serialize(elems, cgilua.errorlog)
-cgilua.errorlog("\n")
---]]
-
 	local func = dispatch (elem_name)
-cgilua.errorlog ("elem_name = "..tostring(elem_name).."\n")
-cgilua.errorlog ("doc = [["..doc.."]]\n")
 	assert (type(func) == "function", "Unavailable method: `"..tostring(elem_name).."'")
 
 	return namespace, func, (elems or {})
@@ -186,20 +119,6 @@ __methods = {
 ---------------------------------------------------------------------
 
 srvMethods (__methods)
---[[
-cgilua.errorlog("cgi:")
-cgilua.errorlog(tostring(cgi))
-c = 0
-for i,v in pairs(cgi) do
-c = c+1
-cgilua.errorlog("#"..c.." ")
-cgilua.errorlog(tostring(i))
-cgilua.errorlog" = "
-cgilua.errorlog(tostring(v))
-cgilua.errorlog"\n"
-end
-cgilua.errorlog("!\n")
---]]
 local namespace, func, arg_table = decodedata (cgi[1])
 local ok, result = callfunc (func, namespace, arg_table)
 respond (result)
