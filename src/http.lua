841774049d45e5c962efca484adcf294905be8cb
tomas <tomas>
2007-07-26 20:25:01 +0000

    Separating the SOAPAction header and the method name

--- src/http.lua
+++ src/http.lua
@@ -1,7 +1,7 @@
 ---------------------------------------------------------------------
 -- SOAP over HTTP.
 -- See Copyright notice in license.html
--- $Id: http.lua,v 1.5 2007/04/11 00:14:28 tomas Exp $
+-- $Id: http.lua,v 1.6 2007/07/26 20:25:01 tomas Exp $
 ---------------------------------------------------------------------
 
 local error, tonumber, tostring = error, tonumber, tostring
@@ -15,6 +15,7 @@ module("soap.http")
 ---------------------------------------------------------------------
 -- Call a remote method.
 -- @param url String with the location of the server.
+-- @param soapaction String with the SOAPAction header value.
 -- @param namespace String with the namespace of the elements.
 -- @param method String with the method's name.
 -- @param entries Table of SOAP elements (LuaExpat's format).
@@ -22,7 +23,7 @@ module("soap.http")
 -- @return String with namespace, String with method's name and
 --	Table with SOAP elements (LuaExpat's format).
 ---------------------------------------------------------------------
-function call (url, namespace, method, entries, headers)
+function call (url, soapaction, namespace, method, entries, headers)
 	local request_sink, tbody = ltn12.sink.table()
 	local request_body = soap.encode(namespace, method, entries, headers)
 	local err, code, headers, status = request {
@@ -33,7 +34,7 @@ function call (url, namespace, method, entries, headers)
 		headers = {
 			["Content-Type"] = "text/xml",
 			["content-length"] = tostring(request_body:len()),
-			["SOAPAction"] = '"'..method..'"',
+			["SOAPAction"] = '"'..soapaction..'"',
 		},
 	}
 	local body = concat(tbody)
