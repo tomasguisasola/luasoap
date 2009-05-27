18256c7db3d992378be3aa388343ab5c975b19d9
tomas <tomas>
2009-05-27 13:02:31 +0000

    Adding client extension. Default is HTTP, but it could be HTTPS also.

--- src/client.lua
+++ src/client.lua
@@ -1,33 +1,34 @@
 ---------------------------------------------------------------------
 -- SOAP over HTTP.
 -- See Copyright notice in license.html
--- $Id: http.lua,v 1.7 2009/03/17 20:27:45 tomas Exp $
+-- $Id: client.lua,v 1.1 2009/05/27 13:02:31 tomas Exp $
 ---------------------------------------------------------------------
 
-local assert, error, tonumber, tostring, pcall = assert, error, tonumber, tostring, pcall
+local assert, tonumber, tostring, pcall, require, print = assert, tonumber, tostring, pcall, require, print
 local concat = require("table").concat
+
 local ltn12 = require("ltn12")
-local request = require("socket.http").request
+local socket_http = require("socket.http")
 local soap = require("soap")
 
-module("soap.http")
+module("soap.client")
+
+http = socket_http
 
 ---------------------------------------------------------------------
 -- Call a remote method.
 -- @param url String with the location of the server.
--- @param soapaction String with the SOAPAction header value.
 -- @param namespace String with the namespace of the elements.
--- @param method_name String with the method's name.
+-- @param method String with the method's name.
 -- @param entries Table of SOAP elements (LuaExpat's format).
 -- @param header Table describing the header of the SOAP-ENV (optional).
--- @return String with namespace.
--- @return String with method's name.
--- @return Table with SOAP elements (LuaExpat's format).
+-- @return String with namespace, String with method's name and
+--	Table with SOAP elements (LuaExpat's format).
 ---------------------------------------------------------------------
-function call(url, soapaction, namespace, method_name, entries, headers)
+function call(url, namespace, method, method_name, entries, headers)
 	local request_sink, tbody = ltn12.sink.table()
 	local request_body = soap.encode(namespace, method_name, entries, headers)
-	local err, code, headers, status = request{
+	url = {
 		url = url,
 		method = "POST",
 		source = ltn12.source.string(request_body),
@@ -35,13 +36,17 @@ function call(url, soapaction, namespace, method_name, entries, headers)
 		headers = {
 			["Content-Type"] = "text/xml",
 			["content-length"] = tostring(request_body:len()),
-			["SOAPAction"] = '"'..soapaction..'"',
+			["SOAPAction"] = '"'..method..'"',
 		},
 	}
+
+	local protocol = url.url:match"^(%a+)" -- obtém o nome do protocolo
+	local mod = assert(_M[protocol], '"'..protocol..'" protocol support not loaded. Try require"soap.client.'..protocol..'" to enable it.')
+	local request = assert(mod.request, 'Could not find request function on module soap.client.'..protocol)
+
+	local err, code, headers, status = request(url)
 	local body = concat(tbody)
-	if tonumber (code) ~= 200 then
-		error (tostring(err or code).."\n\n"..tostring(body))
-	end
+	assert(tonumber(code) == 200, tostring(err or code).."\n\n"..tostring(body))
 
 	local ok, error_or_ns, method, result = pcall(soap.decode, body)
 	assert(ok, tostring(error_or_ns).."\n\n"..tostring(body))
