80632ad5c7dcc115b1a0025aad1555bc4574eebd
Tomas Guisasola <tomasguisasola@gmail.com>
2015-07-24 16:33:51 -0300

    Adaptation to conform to the new API.

--- tests/test-server.lua
+++ tests/test-server.lua
@@ -1,8 +1,8 @@
----------------------------------------------------------------------
--- $Id:$
----------------------------------------------------------------------
+------------------------------------------------------------------------------
+-- SOAP server test.
+------------------------------------------------------------------------------
 
----------------------------------------------------------------------
+------------------------------------------------------------------------------
 -- Fake cgilua support
 
 local function donothing() end
@@ -13,9 +13,40 @@ cgilua = {
 	seterroroutput = donothing, --function(f) f("error message") end,
 }
 
-local server = require("soap.server")
+------------------------------------------------------------------------------
+-- Constants
+local namespace = server_name and string.format("http://%s/path/", server_name)
+	or "http://my.server.name/path/"
+local service_url = namespace.."this_script.lua"
+local disco = string.format([=[<?xml version="1.0" encoding="iso-8859-1" ?>
+<discovery
+	xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	xmlns="http://schemas.xmlsoap.org/disco/">
+<contractRef
+	ref="%s?wsdl"
+	docRef="%s"
+	xmlns="http://schemas.xmlsoap.org/disco/scl/" />
+<soap
+	address="%s"
+	xmlns="listaImagensGaleriaSoapBind"
+	binding="listaImagensGaleriaSoapBind"
+	xmlns="http://schemas.xmlsoap.org/disco/soap/" />
+</discovery>]=], service_url, service_url, service_url)
 
----------------------------------------------------------------------
+------------------------------------------------------------------------------
+-- Server configuration
+local server = require"soap.server".new {
+	encoding = "iso-8859-1",
+	name = "listaImagensGaleria",
+	namespace = namespace,
+	url = service_url,
+	--soap_action = nil, -- ???
+	-- wsdl = nil,
+	disco = disco,
+}
+
+------------------------------------------------------------------------------
 local fake_data = {
 	{
 		{ codigo = 1, sequencial = 2, legenda = 'Bla', ordem = 1, },
@@ -29,7 +60,8 @@ local fake_data = {
 	},
 }
 
----------------------------------------------------------------------
+------------------------------------------------------------------------------
+-- server method implementation
 function lista_imagens_galeria (galeria)
 	assert (galeria, "Nil argument #1 to 'lista_imagens_galeria'")
 	local tg = type(galeria)
@@ -54,32 +86,9 @@ function lista_imagens_galeria (galeria)
 	return dados
 end
 
----------------------------------------------------------------------
-local namespace = server_name and string.format("http://%s/path/", server_name)
-	or "http://my.server.name/path/"
-local script = namespace.."this_script.lua"
-
-local disco = string.format([=[<?xml version="1.0" encoding="iso-8859-1" ?>
-<discovery
-	xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-	xmlns="http://schemas.xmlsoap.org/disco/">
-<contractRef
-	ref="%s?wsdl"
-	docRef="%s"
-	xmlns="http://schemas.xmlsoap.org/disco/scl/" />
-<soap
-	address="%s"
-	xmlns="listaImagensGaleriaSoapBind"
-	binding="listaImagensGaleriaSoapBind"
-	xmlns="http://schemas.xmlsoap.org/disco/soap/" />
-</discovery>]=], script, script, script)
-
-server.register_service_info("listaImagensGaleria",
-	namespace,
-	script, nil, disco)
-
-server.export {
+------------------------------------------------------------------------------
+-- Register server method
+server:export {
 	name = "listaImagensGaleria",
 	method = lista_imagens_galeria,
 	message = {
@@ -98,5 +107,6 @@ server.export {
 	},
 }
 
----------------------------------------------------------------------
-server.handle_request (nil, "wsdl")
+------------------------------------------------------------------------------
+-- Handle request
+server:handle_request (nil, "wsdl")
